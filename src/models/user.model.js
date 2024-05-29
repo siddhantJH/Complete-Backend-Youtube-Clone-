c5a7dee06b91e4778bf1819bdef676d203a5d647
Siddhant jha <40361603+siddhantJH@users.noreply.github.com>
2024-05-29 07:28:46 +0530

    added user and video models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,8 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
+import { jwt } from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
 //agar searching avha karna chahte ho to indexing true kardo 
 const userSchema = new mongoose.Schema({
     watchHistory:{
@@ -42,4 +46,57 @@ const userSchema = new mongoose.Schema({
         required:true
     }
 },{timestamps:true})//created at and updated at automatically mil jaega )
+
+
+
+//directly encrypting and storeing is quite hectic so we need to take help from mongoose hooks(hooks are function)
+//pre hook is a middle  ware  jaise hi data save hone ja raha hoga just usse pehele , ye hook run karwa sakte hai usse pehele save
+//hone se pehele
+//below is the way to run it
+
+
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+    this.password = bcrypt.hash(this.password,10)     //first arg is pass, 2nd pass is rounds
+    next() //jab mai password field ka modification bheju tabhi isko run karna hai,
+            //agar password modification nai hai to run mat karo,then it saves the password as well
+}})
+//this is not a good way as arrow function does not know the context , it takes time so make it async
+//iska kam hai jabhi bhi data save ho raha ho usme se password field ko lo encrpt karke
+//save kardo
+
+
+
+//bcrypt does a lot of task behind the scene , now we will need to add  lot
+//of methods which will taki jab ha, user to import karway usse puch le password sahi hai li nai
+//kyolki db me encrypted save hai aur user jo bahar bhejega vo hoga 1234
+//mongoose gives us lots of methods , so we can make medthodsas well like middleware
+//how to make custom methods
+
+userSchema.methods.isPasswordCorrect = async function(password){
+return await bcrypt.compare(password,this) //used for comparison takes time so make ut await
+}
+
+
+userSchema.methods.generateAccessToken=function(){//JWT ke pass sign methods hai jo generate kar deta hai token
+
+}
+userSchema.methods.generateRefreshToken=function(){
+
+}
+
+//JWT is bearer token means: jo usko bear karta hai usko ham sahi man lete hai
+//means ye token jiske bhi pas hai mai isko data bhej dunga
+
+//next we need to install jwt (json web token)
+//it is based on crypto graphyu which makes token
+//these tokens are usually encrypted
+//JWT.io it has three parts headers , next we have payload(data) , and then we have verification signature
+//JWT and bcrypt is needed in every project
+//session and cookies hamd dono use kar rahe hai to access token database me store nai hoga lekin refresh toke vo database
+//me ham store karenge
+
+//aap ko jitne methods chahiye aap unko schema me inject kar sakte ho
+
 export const User = mongoose.models('user',userSchema)//mongo db me users nam se save hoga
+
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,8 @@
 import mongoose, { Mongoose } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+
 //.model is writeen only for personal convention
 const videoSchema=new mongoose.Schema({
     videoFile:{
@@ -22,14 +26,34 @@ const videoSchema=new mongoose.Schema({
         required:true
     },
     duration:{
-        type:Number,
+        type:Number,//given by cloud nary itself
+        required:true
     },
     views:{
-        type:Number
+        type:Number,
+        default:0
     },
     isPublished:{
         type:Boolean,
+        default:true
     }
 },{timestamps:true})
 
+
+
+videoSchema.plugin(mongooseAggregatePaginate) //we can add plugin as well and add the paginate
 export const Video=mongoose.models('video',videoSchema)
+
+
+
+//saving the watch histroy itself in the code is making this code complex and next level
+//so we need to have an aggreegation pipeline for that to work property
+//for that we will use mongoose-aggregate : it allows us to write aggregation queries
+//first install PS C:\Users\SIDHANT\Desktop\youtubeClone> npm i mongoose-aggregate-paginate-v2
+//then import inside the schema
+
+
+//next install bcrypt js or bcrypt
+//npm i bcrypt
+//both has same work
+//it is a .ibrary which helps us to hash our password
